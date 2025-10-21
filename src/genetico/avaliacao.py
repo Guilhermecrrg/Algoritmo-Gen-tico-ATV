a5085118fa62e4b547258e4669e2f30328c63535
Mateus <@>
2025-10-20 21:13:25 -0300

    torneio

--- src/genetico/avaliacao.py
+++ src/genetico/avaliacao.py
@@ -1,4 +1,5 @@
 from config import matrizCidades
+import random
 
 def gerarCustoCaminhos(caminhos):
     custoCaminhos = []
@@ -11,3 +12,20 @@ def gerarCustoCaminhos(caminhos):
                 custoCaminho += matrizCidades[j][i[num+1]]
         custoCaminhos.append(1/(1+custoCaminho))
     return custoCaminhos
+
+
+def torneio(custos, tamanho_torneio=3):
+    
+    def selecionar_um():
+
+        competidores = random.sample(range(len(custos)), tamanho_torneio)
+        return min(competidores, key=lambda i: custos[i])
+
+    pai1 = selecionar_um()
+    pai2 = selecionar_um()
+
+    # Garante que não sejam o mesmo indivíduo
+    while pai2 == pai1:
+        pai2 = selecionar_um()
+
+    return pai1, pai2
