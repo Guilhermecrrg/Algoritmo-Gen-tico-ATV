1b01348ea6a8a6e2072b812e4ffc76c322b88a17
Mateus <@>
2025-10-20 21:43:29 -0300

    config update

--- src/main.py
+++ src/main.py
@@ -7,19 +7,19 @@ import config
 
 config.matrizCidades = lerMatrizCidades("src/dados.txt")
 print(config.matrizCidades)
-individuos = gerarPopulacaoInicial(config.tamPopulacaoInicial, len(config.matrizCidades))
+config.individuos = gerarPopulacaoInicial(config.tamPopulacaoInicial, len(config.matrizCidades))
 
-custoCaminhos = gerarCustoCaminhos(individuos)
+custoCaminhos = gerarCustoCaminhos(config.individuos)
 #print(custoCaminhos)
 
 pos1, pos2 = torneio(custoCaminhos)
 
 # print(individuos[0][:99])
 
-print('Matriz1 ', individuos[pos1])
-print('Matriz2 ', individuos[pos2])
+print('Matriz1 ', config.individuos[pos1])
+print('Matriz2 ', config.individuos[pos2])
 
-filho1, filho2 = crossoverOxDuplo(individuos[pos1],individuos[pos2])
+filho1, filho2 = crossoverOxDuplo(config.individuos[pos1],config.individuos[pos2])
 print('filho1 ', filho1)
 print('filho2 ', filho2)
 
