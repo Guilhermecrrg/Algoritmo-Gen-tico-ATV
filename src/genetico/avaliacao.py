d85b114c02db42a84ce9b4d713ff9ae53b8b5a7d
Guilhermecrrg <guilhermecrrg@gmail.com>
2025-10-20 21:41:55 -0300

    mutacao

--- src/genetico/avaliacao.py
+++ src/genetico/avaliacao.py
@@ -1,21 +1,19 @@
 import config
 import random
 
-def gerarCustoCaminhos(caminhos):
-    custoCaminhos = []
-    for i in caminhos:
-        custoCaminho = 0
+def gerarCustoCaminhos(individuos):
+    custoIndividuos = []
+    for i in individuos:
+        custoIndividuo = 0
         for num, j in enumerate(i[:-1]):
             if config.matrizCidades[j][i[num+1]] == 0:
-                custoCaminho += 9999
+                custoIndividuo += 9999
             else:
-                custoCaminho += config.matrizCidades[j][i[num+1]]
-        custoCaminhos.append(1/(1+custoCaminho))
-    return custoCaminhos
-
+                custoIndividuo += config.matrizCidades[j][i[num+1]]
+        custoIndividuos.append(1/(1+custoIndividuo))
+    return custoIndividuos
 
 def torneio(custos, tamanho_torneio=3):
-    
     def selecionar_um():
 
         competidores = random.sample(range(len(custos)), tamanho_torneio)
